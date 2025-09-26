977c066f8a05132912c1f7920eedeb3d04dfecb2
huyle <levanhuy2k4@gmail.com>
2025-09-26 10:31:51 +0700

    update

--- src/components/QRScanner.js
+++ src/components/QRScanner.js
@@ -1,82 +1,36 @@
 import React, { useEffect } from "react";
 import { BrowserMultiFormatReader } from "@zxing/library";
-import { Capacitor } from "@capacitor/core";
-import { Camera, CameraResultType } from "@capacitor/camera";
-import jsQR from "jsqr";
 
-const QRScanner = ({ onScan, scannedList }) => {
+const QRScanner = ({ onScan }) => {
   useEffect(() => {
-    if (Capacitor.isNativePlatform()) {
-      // --- Mobile: dùng Camera Capacitor ---
-      const scanWithCamera = async () => {
-        try {
-          const photo = await Camera.getPhoto({
-            resultType: CameraResultType.Uri,
-            source: "CAMERA",
-          });
+    const codeReader = new BrowserMultiFormatReader();
+    let selectedDeviceId;
 
-          const image = new Image();
-          image.src = photo.webPath;
-          image.onload = () => {
-            const canvas = document.createElement("canvas");
-            canvas.width = image.width;
-            canvas.height = image.height;
-            const ctx = canvas.getContext("2d");
-            ctx.drawImage(image, 0, 0, image.width, image.height);
-            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-            const code = jsQR(imageData.data, canvas.width, canvas.height);
-            if (code && code.data.startsWith("ATTEND:")) {
-              const mssv = code.data.split(":")[1];
-              if (scannedList.includes(mssv)) {
-                alert(`MSSV ${mssv} đã được điểm danh!`);
-              } else {
-                onScan(mssv);
-              }
-            }
-          };
-        } catch (err) {
-          console.error(err);
+    codeReader
+      .listVideoInputDevices()
+      .then((videoInputDevices) => {
+        selectedDeviceId = videoInputDevices[0]?.deviceId;
+        return codeReader.decodeOnceFromVideoDevice(selectedDeviceId, "video");
+      })
+      .then((result) => {
+        if (result.text.startsWith("ATTEND:")) {
+          const mssv = result.text.split(":")[1];
+          onScan(mssv);
         }
-      };
+      })
+      .catch((err) => console.error(err));
 
-      scanWithCamera();
-    } else {
-      // --- Web/Desktop: dùng video stream ---
-      const codeReader = new BrowserMultiFormatReader();
-      let selectedDeviceId;
-
-      codeReader
-        .listVideoInputDevices()
-        .then((videoInputDevices) => {
-          selectedDeviceId = videoInputDevices[0]?.deviceId;
-          return codeReader.decodeOnceFromVideoDevice(selectedDeviceId, "video");
-        })
-        .then((result) => {
-          if (result.text.startsWith("ATTEND:")) {
-            const mssv = result.text.split(":")[1];
-            if (scannedList.includes(mssv)) {
-              alert(`MSSV ${mssv} đã được điểm danh!`);
-            } else {
-              onScan(mssv);
-            }
-          }
-        })
-        .catch((err) => console.error(err));
-
-      return () => codeReader.reset();
-    }
-  }, [onScan, scannedList]);
+    return () => codeReader.reset();
+  }, [onScan]);
 
   return (
     <div className="flex justify-center my-4">
-      {!Capacitor.isNativePlatform() && (
-        <video
-          id="video"
-          className="border-4 border-blue-500 rounded-lg shadow-lg"
-          width="300"
-          height="200"
-        ></video>
-      )}
+      <video
+        id="video"
+        className="border-4 border-blue-500 rounded-lg shadow-lg"
+        width="300"
+        height="200"
+      ></video>
     </div>
   );
 };
